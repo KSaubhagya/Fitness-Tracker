edf85a96fe1422872580f80a1db94b2e1bb9547f
KSaubhagya <saubhagyakrhmk.21@uom.lk>
2024-12-17 19:18:48 +0530

    homepage

--- fitness-tracker/app/(tabs)/index.tsx
+++ fitness-tracker/app/(tabs)/index.tsx
@@ -1,27 +1,44 @@
-import { StyleSheet } from 'react-native';
-
+import React from "react";
+import { SafeAreaView } from 'react-native';
+import { Text,View,StyleSheet,TouchableOpacity } from "react-native";
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+
+import { useLayoutEffect } from 'react';
+import { blue, red } from "react-native-reanimated/lib/typescript/Colors";
+
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Homepage</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>track 2</Text>
+    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    //   <EditScreenInfo path="app/(tabs)/index.tsx" />
+    // </View>
+    <SafeAreaView style={styles.safe}>
+     <View >
+          <Text>Enjoy the Life</Text>
+          <Text>with good health</Text>
+        
     </View>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  safe:{
+    flex: 1,
+   
+   
   },
   separator: {
     marginVertical: 30,
